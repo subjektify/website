ecf323a7027ea11aa3141001eef8f573b962dc6b
Mohamed Amer <139323704+atmohamedamer@users.noreply.github.com>
2024-05-30 00:12:10 -0700

    Create tools.tsx

---
+++ src/pages/tools.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className="container">
+                <h1 className="hero__title">{siteConfig.title}</h1>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--success button--lg margin-right--sm"
+                        href='https://composer.subjektify.dev/'>
+                        Start building 🚀
+                    </Link>
+                    <Link
+                        className="button button--secondary button--lg"
+                        to="/docs/learn#fast-track">
+                        Tutorial - 5 min ⏱️
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default function Tools(): JSX.Element {
+    return (
+        <HomepageHeader />
+    );
+}
